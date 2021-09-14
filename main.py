305b0d240e4f24a33aef0fcbeb6177f88832840d
Kathy Qian <hello@kathyqian.com>
2021-09-13 20:52:44 -0700

    bump default cycle

--- main.py
+++ main.py
@@ -55,8 +55,8 @@ def get_years():
             "max": datetime.datetime.now().year
         },
         "default": {
-            "min": 2019,
-            "max": 2020
+            "min": 2021,
+            "max": 2022
         }
     }
 
@@ -65,7 +65,7 @@ def get_cycles():
     return {
         "min": 2016,
         "max": math.ceil(datetime.datetime.now().year/2.)*2,
-        "current": 2020
+        "current": 2022
     }
 
 #########################################################
