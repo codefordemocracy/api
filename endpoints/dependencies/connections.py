a435add9f7f14d2923a7ae17ffd30c7df04d3982
Kathy Qian <hello@kathyqian.com>
2021-10-04 21:47:49 -0400

    increase elasticsearch timeout

--- endpoints/dependencies/connections.py
+++ endpoints/dependencies/connections.py
@@ -12,7 +12,7 @@ from . import credentials
 driver = GraphDatabase.driver(credentials.neo4j_connection, auth=(credentials.neo4j_username_api, credentials.neo4j_password_api))
 
 # elasticsearch
-es = Elasticsearch(credentials.elastic_host, http_auth=(credentials.elastic_username_api, credentials.elastic_password_api), scheme="https", port=443)
+es = Elasticsearch(credentials.elastic_host, http_auth=(credentials.elastic_username_api, credentials.elastic_password_api), scheme="https", port=443, timeout=30, retry_on_timeout=True)
 
 # firestore
 db = firestore.Client()
