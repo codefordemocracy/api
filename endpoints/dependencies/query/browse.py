bdce536bd8514817c871b3f59f2db9e10a1e6bfd
Kathy Qian <hello@kathyqian.com>
2021-10-07 15:12:48 -0400

    update responses from browse endpoints

--- endpoints/dependencies/query/browse.py
+++ endpoints/dependencies/query/browse.py
@@ -21,10 +21,16 @@ def documents_browse_news_articles_source(es, domains, text, histogram, skip, li
                 "extracted.source.url": domains
             }
         })
-    return get_response(es, "news_articles", q, skip, limit, False, histogram,
+    response = get_response(es, "news_articles", q, skip, limit, False, histogram,
         date_field="extracted.date", mindate=mindate, maxdate=maxdate,
         filter_path=["hits.hits._id", "hits.hits._source.extracted.title", "hits.hits._source.extracted.text", "hits.hits._source.extracted.date", "hits.hits._source.extracted.url"]
     )
+    if histogram is not True:
+        elements = []
+        for hit in response:
+            elements.append(hit["_source"])
+        return elements
+    return response
 
 def documents_browse_twitter_tweets_user(es, user_ids, text, histogram, skip, limit, mindate, maxdate):
     q = make_query()
@@ -46,10 +52,16 @@ def documents_browse_twitter_tweets_user(es, user_ids, text, histogram, skip, li
                 "obj.author.id": user_ids
             }
         })
-    return get_response(es, "twitter_tweets_new", q, skip, limit, False, histogram,
+    response = get_response(es, "twitter_tweets_new", q, skip, limit, False, histogram,
         date_field="obj.tweet.created_at", mindate=mindate, maxdate=maxdate,
         filter_path=["hits.hits._source.obj.author.id", "hits.hits._source.obj.author.username", "hits.hits._source.obj.tweet.id", "hits.hits._source.obj.tweet.created_at", "hits.hits._source.obj.tweet.entities.hashtags"]
     )
+    if histogram is not True:
+        elements = []
+        for hit in response:
+            elements.append(hit["_source"])
+        return elements
+    return response
 
 def documents_browse_facebook_ads(es, text, histogram, skip, limit, mindate, maxdate):
     q = make_query()
@@ -80,7 +92,13 @@ def documents_browse_facebook_ads(es, text, histogram, skip, limit, mindate, max
                 "minimum_should_match": 1
             }
         })
-    return get_response(es, "facebook_ads", q, skip, limit, False, histogram,
+    response = get_response(es, "facebook_ads", q, skip, limit, False, histogram,
         date_field="obj.ad_creation_time", mindate=mindate, maxdate=maxdate,
         filter_path=["hits.hits._source.obj.id", "hits.hits._source.obj.page_id", "hits.hits._source.obj.page_name", "hits.hits._source.obj.funding_entity", "hits.hits._source.obj.ad_creation_time"]
     )
+    if histogram is not True:
+        elements = []
+        for hit in response:
+            elements.append(hit["_source"])
+        return elements
+    return response
