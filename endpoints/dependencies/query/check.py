e033ab08cd59d68c669a84d8459723087eeb1eb4
Kathy Qian <hello@kathyqian.com>
2021-10-26 11:00:30 -0700

    add update cadence to status page

--- endpoints/dependencies/query/check.py
+++ endpoints/dependencies/query/check.py
@@ -18,6 +18,34 @@ def status_check_data_articles(es):
         },
         filter_path=["aggregations"]
     )
+    cadence = es.search(index="news_articles",
+        body={
+            "size": 0,
+            "query": {
+                "bool": {
+                    "filter": [
+                        {
+                            "range": {
+                                "context.last_indexed": {
+                                    "gte": datetime.datetime.now(pytz.timezone('US/Eastern'))-datetime.timedelta(days=90)
+                                }
+                            }
+                        }
+                    ]
+                }
+            },
+            "aggs": {
+                "dates": {
+                    "date_histogram": {
+                        "field": "context.last_indexed",
+                        "calendar_interval": "day",
+                        "time_zone": "America/New_York"
+                    }
+                }
+            }
+        },
+        filter_path=["aggregations"]
+    )
     last_indexed = es.search(index="news_articles",
         body={
             "size": 0,
@@ -34,6 +62,7 @@ def status_check_data_articles(es):
     return {
         "count": count["count"],
         "coverage": coverage["aggregations"]["dates"]["buckets"],
+        "cadence": cadence["aggregations"]["dates"]["buckets"],
         "last_indexed": pytz.utc.localize(datetime.datetime.fromtimestamp(last_indexed["aggregations"]["last_indexed"]["value"]/1000)).astimezone(tz=pytz.timezone('US/Eastern'))
     }
 
@@ -47,6 +76,34 @@ def status_check_data_ads(es):
                     "date_histogram": {
                         "field": "obj.ad_creation_time",
                         "calendar_interval": "quarter",
+                        "time_zone": "America/New_York"
+                    }
+                }
+            }
+        },
+        filter_path=["aggregations"]
+    )
+    cadence = es.search(index="facebook_ads",
+        body={
+            "size": 0,
+            "query": {
+                "bool": {
+                    "filter": [
+                        {
+                            "range": {
+                                "context.last_indexed": {
+                                    "gte": datetime.datetime.now(pytz.timezone('US/Eastern'))-datetime.timedelta(days=90)
+                                }
+                            }
+                        }
+                    ]
+                }
+            },
+            "aggs": {
+                "dates": {
+                    "date_histogram": {
+                        "field": "context.last_indexed",
+                        "calendar_interval": "day",
                         "time_zone": "America/New_York"
                     }
                 }
@@ -70,6 +127,7 @@ def status_check_data_ads(es):
     return {
         "count": count["count"],
         "coverage": coverage["aggregations"]["dates"]["buckets"],
+        "cadence": cadence["aggregations"]["dates"]["buckets"],
         "last_indexed": pytz.utc.localize(datetime.datetime.fromtimestamp(last_indexed["aggregations"]["last_indexed"]["value"]/1000)).astimezone(tz=pytz.timezone('US/Eastern'))
     }
 
@@ -104,6 +162,34 @@ def status_check_data_contributions(es):
         },
         filter_path=["aggregations"],
         request_timeout=30
+    )
+    bulk_cadence = es.search(index="federal_fec_contributions",
+        body={
+            "size": 0,
+            "query": {
+                "bool": {
+                    "filter": [
+                        {
+                            "range": {
+                                "context.last_bulked": {
+                                    "gte": datetime.datetime.now(pytz.timezone('US/Eastern'))-datetime.timedelta(days=90)
+                                }
+                            }
+                        }
+                    ]
+                }
+            },
+            "aggs": {
+                "dates": {
+                    "date_histogram": {
+                        "field": "context.last_bulked",
+                        "calendar_interval": "day",
+                        "time_zone": "America/New_York"
+                    }
+                }
+            }
+        },
+        filter_path=["aggregations"]
     )
     bulk_last_indexed = es.search(index="federal_fec_contributions",
         body={
@@ -154,6 +240,34 @@ def status_check_data_contributions(es):
         filter_path=["aggregations"],
         request_timeout=30
     )
+    api_cadence = es.search(index="federal_fec_contributions",
+        body={
+            "size": 0,
+            "query": {
+                "bool": {
+                    "filter": [
+                        {
+                            "range": {
+                                "context.last_augmented": {
+                                    "gte": datetime.datetime.now(pytz.timezone('US/Eastern'))-datetime.timedelta(days=90)
+                                }
+                            }
+                        }
+                    ]
+                }
+            },
+            "aggs": {
+                "dates": {
+                    "date_histogram": {
+                        "field": "context.last_augmented",
+                        "calendar_interval": "day",
+                        "time_zone": "America/New_York"
+                    }
+                }
+            }
+        },
+        filter_path=["aggregations"]
+    )
     api_last_indexed = es.search(index="federal_fec_contributions",
         body={
             "size": 0,
@@ -178,11 +292,13 @@ def status_check_data_contributions(es):
         "bulk": {
             "count": bulk_count["count"],
             "coverage": bulk_coverage["aggregations"]["dates"]["buckets"],
+            "cadence": bulk_cadence["aggregations"]["dates"]["buckets"],
             "last_indexed": pytz.utc.localize(datetime.datetime.fromtimestamp(bulk_last_indexed["aggregations"]["last_indexed"]["value"]/1000)).astimezone(tz=pytz.timezone('US/Eastern'))
         },
         "api": {
             "count": api_count["count"],
             "coverage": api_coverage["aggregations"]["dates"]["buckets"],
+            "cadence": api_cadence["aggregations"]["dates"]["buckets"],
             "last_indexed": pytz.utc.localize(datetime.datetime.fromtimestamp(api_last_indexed["aggregations"]["last_indexed"]["value"]/1000)).astimezone(tz=pytz.timezone('US/Eastern'))
         }
     }
@@ -197,6 +313,34 @@ def status_check_data_lobbying(es):
                     "date_histogram": {
                         "field": "processed.date_submitted",
                         "calendar_interval": "quarter",
+                        "time_zone": "America/New_York"
+                    }
+                }
+            }
+        },
+        filter_path=["aggregations"]
+    )
+    senate_disclosures_cadence = es.search(index="federal_senate_lobbying_disclosures",
+        body={
+            "size": 0,
+            "query": {
+                "bool": {
+                    "filter": [
+                        {
+                            "range": {
+                                "context.last_indexed": {
+                                    "gte": datetime.datetime.now(pytz.timezone('US/Eastern'))-datetime.timedelta(days=90)
+                                }
+                            }
+                        }
+                    ]
+                }
+            },
+            "aggs": {
+                "dates": {
+                    "date_histogram": {
+                        "field": "context.last_indexed",
+                        "calendar_interval": "day",
                         "time_zone": "America/New_York"
                     }
                 }
@@ -233,6 +377,34 @@ def status_check_data_lobbying(es):
         },
         filter_path=["aggregations"]
     )
+    house_disclosures_cadence = es.search(index="federal_house_lobbying_disclosures",
+        body={
+            "size": 0,
+            "query": {
+                "bool": {
+                    "filter": [
+                        {
+                            "range": {
+                                "context.last_indexed": {
+                                    "gte": datetime.datetime.now(pytz.timezone('US/Eastern'))-datetime.timedelta(days=90)
+                                }
+                            }
+                        }
+                    ]
+                }
+            },
+            "aggs": {
+                "dates": {
+                    "date_histogram": {
+                        "field": "context.last_indexed",
+                        "calendar_interval": "day",
+                        "time_zone": "America/New_York"
+                    }
+                }
+            }
+        },
+        filter_path=["aggregations"]
+    )
     house_disclosures_last_indexed = es.search(index="federal_house_lobbying_disclosures",
         body={
             "size": 0,
@@ -255,6 +427,34 @@ def status_check_data_lobbying(es):
                     "date_histogram": {
                         "field": "processed.date_submitted",
                         "calendar_interval": "quarter",
+                        "time_zone": "America/New_York"
+                    }
+                }
+            }
+        },
+        filter_path=["aggregations"]
+    )
+    senate_contributions_cadence = es.search(index="federal_senate_lobbying_contributions",
+        body={
+            "size": 0,
+            "query": {
+                "bool": {
+                    "filter": [
+                        {
+                            "range": {
+                                "context.last_indexed": {
+                                    "gte": datetime.datetime.now(pytz.timezone('US/Eastern'))-datetime.timedelta(days=90)
+                                }
+                            }
+                        }
+                    ]
+                }
+            },
+            "aggs": {
+                "dates": {
+                    "date_histogram": {
+                        "field": "context.last_indexed",
+                        "calendar_interval": "day",
                         "time_zone": "America/New_York"
                     }
                 }
@@ -291,6 +491,34 @@ def status_check_data_lobbying(es):
         },
         filter_path=["aggregations"]
     )
+    house_contributions_cadence = es.search(index="federal_house_lobbying_contributions",
+        body={
+            "size": 0,
+            "query": {
+                "bool": {
+                    "filter": [
+                        {
+                            "range": {
+                                "context.last_indexed": {
+                                    "gte": datetime.datetime.now(pytz.timezone('US/Eastern'))-datetime.timedelta(days=90)
+                                }
+                            }
+                        }
+                    ]
+                }
+            },
+            "aggs": {
+                "dates": {
+                    "date_histogram": {
+                        "field": "context.last_indexed",
+                        "calendar_interval": "day",
+                        "time_zone": "America/New_York"
+                    }
+                }
+            }
+        },
+        filter_path=["aggregations"]
+    )
     house_contributions_last_indexed = es.search(index="federal_house_lobbying_contributions",
         body={
             "size": 0,
@@ -309,11 +537,13 @@ def status_check_data_lobbying(es):
             "disclosures": {
                 "count": senate_disclosures_count["count"],
                 "coverage": senate_disclosures_coverage["aggregations"]["dates"]["buckets"],
+                "cadence": senate_disclosures_cadence["aggregations"]["dates"]["buckets"],
                 "last_indexed": pytz.utc.localize(datetime.datetime.fromtimestamp(senate_disclosures_last_indexed["aggregations"]["last_indexed"]["value"]/1000)).astimezone(tz=pytz.timezone('US/Eastern'))
             },
             "contributions": {
                 "count": senate_contributions_count["count"],
                 "coverage": senate_contributions_coverage["aggregations"]["dates"]["buckets"],
+                "cadence": senate_contributions_cadence["aggregations"]["dates"]["buckets"],
                 "last_indexed": pytz.utc.localize(datetime.datetime.fromtimestamp(senate_contributions_last_indexed["aggregations"]["last_indexed"]["value"]/1000)).astimezone(tz=pytz.timezone('US/Eastern'))
             }
         },
@@ -321,11 +551,13 @@ def status_check_data_lobbying(es):
             "disclosures": {
                 "count": house_disclosures_count["count"],
                 "coverage": house_disclosures_coverage["aggregations"]["dates"]["buckets"],
+                "cadence": house_disclosures_cadence["aggregations"]["dates"]["buckets"],
                 "last_indexed": pytz.utc.localize(datetime.datetime.fromtimestamp(house_disclosures_last_indexed["aggregations"]["last_indexed"]["value"]/1000)).astimezone(tz=pytz.timezone('US/Eastern'))
             },
             "contributions": {
                 "count": house_contributions_count["count"],
                 "coverage": house_contributions_coverage["aggregations"]["dates"]["buckets"],
+                "cadence": house_contributions_cadence["aggregations"]["dates"]["buckets"],
                 "last_indexed": pytz.utc.localize(datetime.datetime.fromtimestamp(house_contributions_last_indexed["aggregations"]["last_indexed"]["value"]/1000)).astimezone(tz=pytz.timezone('US/Eastern'))
             }
         }
@@ -341,6 +573,34 @@ def status_check_data_990(es):
                     "date_histogram": {
                         "field": "row.sub_date",
                         "calendar_interval": "quarter",
+                        "time_zone": "America/New_York"
+                    }
+                }
+            }
+        },
+        filter_path=["aggregations"]
+    )
+    cadence = es.search(index="federal_irs_990,federal_irs_990ez,federal_irs_990pf",
+        body={
+            "size": 0,
+            "query": {
+                "bool": {
+                    "filter": [
+                        {
+                            "range": {
+                                "context.last_indexed": {
+                                    "gte": datetime.datetime.now(pytz.timezone('US/Eastern'))-datetime.timedelta(days=90)
+                                }
+                            }
+                        }
+                    ]
+                }
+            },
+            "aggs": {
+                "dates": {
+                    "date_histogram": {
+                        "field": "context.last_indexed",
+                        "calendar_interval": "day",
                         "time_zone": "America/New_York"
                     }
                 }
@@ -364,5 +624,6 @@ def status_check_data_990(es):
     return {
         "count": count["count"],
         "coverage": coverage["aggregations"]["dates"]["buckets"],
+        "cadence": cadence["aggregations"]["dates"]["buckets"],
         "last_indexed": pytz.utc.localize(datetime.datetime.fromtimestamp(last_indexed["aggregations"]["last_indexed"]["value"]/1000)).astimezone(tz=pytz.timezone('US/Eastern'))
     }
